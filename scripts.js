caf606685e81d7989ae11578f9c7058a050b44a0
subham-kumar12 <subham10112005@gmail.com>
2026-06-29 23:33:32 +0530

    update nav bar 1st time

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.site-nav');
 if (navToggle && navMenu) {
@@ -6,12 +5,3 @@ if (navToggle && navMenu) {
     navMenu.classList.toggle('open');
   });
 }
-=======
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.site-nav');
-if (navToggle && navMenu) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-  });
-}
->>>>>>> 4ce63ceba073592a46e1aa8c3dcee76618d10f5d
